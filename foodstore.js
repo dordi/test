f12f5c3a2f91c7e87efa516f9a8257d2e33af657
Aymanov1 <aymoun0002@gmail.com>
2014-12-23 01:53:47 +0300

    Update foodstore.js

--- foodstore.js
+++ foodstore.js
@@ -4,10 +4,12 @@ function createXmlHttpRequestObject() {
     var xmlHttp;
     if (window.ActiveXObject) {
         try {
+            
             xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
         }catch (e){
             xmlHttp = false;
         }
+        
     }else {
         try {
             xmlHttp = new XMLHttpRequest();
