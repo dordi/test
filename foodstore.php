29c09a6bdab796267111c28b8a8a0f837a3783e9
dordi <douraid.tlili1991@gmail.com>
2014-12-22 02:33:00 +0100

    Create foodstore.php

---
+++ foodstore.php
@@ -0,0 +1,15 @@
+<?php
+
+header("Content-Type: text/xml");
+echo "<?xml version='1.0' encoding='UTF-8' standalone='yes'?>";
+echo "<response>";
+    $food = $_GET['food'];
+    $foodArray = array('tuna', 'bacon', 'beef', 'loaf', 'ham');
+    if (in_array($food, $foodArray)) {
+        echo 'we do have ' . $food . '!';
+    } elseif ($food == '') {
+        echo 'nothing entred';
+    } else {
+        echo 'sorry we do not sell any ' . $food;
+    }
+echo "</response>";
